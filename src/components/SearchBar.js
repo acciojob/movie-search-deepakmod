738e333071f9954006d2a043f039bc096b315a01
Deepak Modanwal <deepakmodanwal404@gmail.com>
2023-06-20 09:19:41 +0530

    lst

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,16 +6,17 @@ const SearchBar = () => {
     let [movieName, setMovieName]=useState('');
     let [moviesList, setMoviesList]=useState([]);
 
-    function showMovies(){;
+    function showMovies(event){
+        event.preventDefault();
         axios.get(`https://www.omdbapi.com/?s=${movieName}&apikey=1cc2a4e0`)
         .then(res=>(setMoviesList(res.data.Search)));
     }
 
     return (
-        <div>
+        <form>
             <p>Search Movie</p>
             <input type="text" value={movieName} onChange={(e)=>setMovieName(e.target.value)} />
-            <button onClick={()=>{showMovies()}}>Search</button>
+            <button onClick={(e)=>{showMovies(e)}}>Search</button>
 
             {moviesList ? 
             <ul>
@@ -26,7 +27,7 @@ const SearchBar = () => {
                     </li>))}
             </ul>:
             <p className='error'>Invalid movie name. Please try again.</p>}
-        </div>
+        </form>
     );
 };
 
